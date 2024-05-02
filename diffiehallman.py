55567deaab71f354b5ac74be3c64adb003c367d2
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-05-02 23:03:45 +0530

    Updated

--- diffiehallman.py
+++ diffiehallman.py
@@ -1,67 +1,64 @@
-from random import randint
+# Diffie-Hellman Code
 
 
-def generate_prime(bits):
-    # Generate a random number of 'bits' length
-    num = randint(2 ** (bits - 1), 2**bits - 1)
-    # Keep generating until a prime number is found
-    while not is_prime(num):
-        num = randint(2 ** (bits - 1), 2**bits - 1)
-    return num
+def prime_checker(p):
+    # Checks If the number entered is a Prime Number or not
+    if p < 1:
+        return -1
+    elif p > 1:
+        if p == 2:
+            return 1
+        for i in range(2, p):
+            if p % i == 0:
+                return -1
+            return 1
 
 
-def is_prime(n, k=5):
-    if n == 2 or n == 3:
-        return True
-    if n <= 1 or n % 2 == 0:
-        return False
-    # Write n as 2^r * d + 1
-    r, d = 0, n - 1
-    while d % 2 == 0:
-        r += 1
-        d //= 2
-    # Witness loop
-    for _ in range(k):
-        a = randint(2, n - 1)
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
-    return True
+def primitive_check(g, p, L):
+    # Checks If The Entered Number Is A Primitive Root Or Not
+    for i in range(1, p):
+        L.append(pow(g, i) % p)
+    for i in range(1, p):
+        if L.count(i) > 1:
+            L.clear()
+            return -1
+        return 1
 
 
-def primitive_root(prime):
-    primitive_roots = []
-    for a in range(2, prime):
-        if len(primitive_roots) > 0:
-            return primitive_roots
-        for i in range(1, prime):
-            if (a**i) % prime == 1:
-                break
-        else:
-            primitive_roots.append(a)
+l = []
+while 1:
+    P = int(input("Enter P : "))
+    if prime_checker(P) == -1:
+        print("Number Is Not Prime, Please Enter Again!")
+        continue
+    break
 
+while 1:
+    G = int(input(f"Enter The Primitive Root Of {P} : "))
+    if primitive_check(G, P, l) == -1:
+        print(f"Number Is Not A Primitive Root Of {P}, Please Try Again!")
+        continue
+    break
 
-def diffie_hellman(prime_length):
-    prime = generate_prime(prime_length)
-    primitive_roots = primitive_root(prime)
-    g = primitive_roots[0]
-    private_key_a = randint(2, prime - 1)
-    public_key_a = pow(g, private_key_a, prime)
-    private_key_b = randint(2, prime - 1)
-    public_key_b = pow(g, private_key_b, prime)
-    shared_secret_a = pow(public_key_b, private_key_a, prime)
-    shared_secret_b = pow(public_key_a, private_key_b, prime)
-    return shared_secret_a, shared_secret_b
+# Private Keys
+x1, x2 = int(input("Enter The Private Key Of User 1 : ")), int(
+    input("Enter The Private Key Of User 2 : ")
+)
+while 1:
+    if x1 >= P or x2 >= P:
+        print(f"Private Key Of Both The Users Should Be Less Than {P}!")
+        continue
+    break
 
+# Calculate Public Keys
+y1, y2 = pow(G, x1) % P, pow(G, x2) % P
 
-# Example Usage:
-prime_length = 256
-shared_secret_a, shared_secret_b = diffie_hellman(prime_length)
-print("Shared Secret A:", shared_secret_a)
-print("Shared Secret B:", shared_secret_b)
+# Generate Secret Keys
+k1, k2 = pow(y2, x1) % P, pow(y1, x2) % P
+
+print(f"\nSecret Key For User 1 Is {k1}\nSecret Key For User 2 Is {k2}\n")
+
+if k1 == k2:
+    print("Keys Have Been Exchanged Successfully")
+else:
+    print("Keys Have Not Been Exchanged Successfully")
--- playfair.py
+++ playfair.py
@@ -1,45 +1,205 @@
-def playfair(text, key, decrypt=False):
-    def create_matrix(key):
-        print("Key before sorting:", key)
-        key = "".join(sorted(set(key.replace("J", "I") + "ABCDEFGHIKLMNOPQRSTUVWXYZ")))
-        print("Sorted key:", key)
-        return [list(key[i : i + 5]) for i in range(0, 25, 5)]
+# Python program to implement Playfair Cipher
 
-    def find_coords(matrix, char):
-        return [
-            (r, c)
-            for r, row in enumerate(matrix)
-            for c, val in enumerate(row)
-            if val == char
-        ]
+# Function to convert the string to lowercase
 
-    def process_pair(matrix, a, b):
-        (a_row, a_col), (b_row, b_col) = find_coords(matrix, a), find_coords(matrix, b)
-        if a_row == b_row:
-            return (
-                matrix[a_row][(a_col + (-1 if decrypt else 1)) % 5]
-                + matrix[b_row][(b_col + (-1 if decrypt else 1)) % 5]
-            )
-        elif a_col == b_col:
-            return (
-                matrix[(a_row + (-1 if decrypt else 1)) % 5][a_col]
-                + matrix[(b_row + (-1 if decrypt else 1)) % 5][b_col]
-            )
+
+def toLowerCase(text):
+    return text.lower()
+
+
+# Function to remove all spaces in a string
+
+
+def removeSpaces(text):
+    newText = ""
+    for i in text:
+        if i == " ":
+            continue
         else:
-            return matrix[a_row][b_col] + matrix[b_row][a_col]
-
-    text = text.replace("J", "I").replace(" ", "").upper()
-    matrix = create_matrix(key)
-    processed_text = ""
-    for i in range(0, len(text), 2):
-        processed_text += process_pair(matrix, text[i], text[i + 1])
-    return processed_text
+            newText = newText + i
+    return newText
 
 
-# Example Usage:
-key = "playfair example"
-plaintext = "Hide the gold in the tree stump"
-encrypted_text = playfair(plaintext, key)
-print("Encrypted:", encrypted_text)
-decrypted_text = playfair(encrypted_text, key, decrypt=True)
-print("Decrypted:", decrypted_text)
+# Function to group 2 elements of a string
+# as a list element
+
+
+def Diagraph(text):
+    Diagraph = []
+    group = 0
+    for i in range(2, len(text), 2):
+        Diagraph.append(text[group:i])
+
+        group = i
+    Diagraph.append(text[group:])
+    return Diagraph
+
+
+# Function to fill a letter in a string element
+# If 2 letters in the same string matches
+
+
+def FillerLetter(text):
+    k = len(text)
+    if k % 2 == 0:
+        for i in range(0, k, 2):
+            if text[i] == text[i + 1]:
+                new_word = text[0 : i + 1] + str("x") + text[i + 1 :]
+                new_word = FillerLetter(new_word)
+                break
+            else:
+                new_word = text
+    else:
+        for i in range(0, k - 1, 2):
+            if text[i] == text[i + 1]:
+                new_word = text[0 : i + 1] + str("x") + text[i + 1 :]
+                new_word = FillerLetter(new_word)
+                break
+            else:
+                new_word = text
+    return new_word
+
+
+list1 = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+# Function to generate the 5x5 key square matrix
+
+
+def generateKeyTable(word, list1):
+    key_letters = []
+    for i in word:
+        if i not in key_letters:
+            key_letters.append(i)
+
+    compElements = []
+    for i in key_letters:
+        if i not in compElements:
+            compElements.append(i)
+    for i in list1:
+        if i not in compElements:
+            compElements.append(i)
+
+    matrix = []
+    while compElements != []:
+        matrix.append(compElements[:5])
+        compElements = compElements[5:]
+
+    return matrix
+
+
+def search(mat, element):
+    for i in range(5):
+        for j in range(5):
+            if mat[i][j] == element:
+                return i, j
+
+
+def encrypt_RowRule(matr, e1r, e1c, e2r, e2c):
+    char1 = ""
+    if e1c == 4:
+        char1 = matr[e1r][0]
+    else:
+        char1 = matr[e1r][e1c + 1]
+
+    char2 = ""
+    if e2c == 4:
+        char2 = matr[e2r][0]
+    else:
+        char2 = matr[e2r][e2c + 1]
+
+    return char1, char2
+
+
+def encrypt_ColumnRule(matr, e1r, e1c, e2r, e2c):
+    char1 = ""
+    if e1r == 4:
+        char1 = matr[0][e1c]
+    else:
+        char1 = matr[e1r + 1][e1c]
+
+    char2 = ""
+    if e2r == 4:
+        char2 = matr[0][e2c]
+    else:
+        char2 = matr[e2r + 1][e2c]
+
+    return char1, char2
+
+
+def encrypt_RectangleRule(matr, e1r, e1c, e2r, e2c):
+    char1 = ""
+    char1 = matr[e1r][e2c]
+
+    char2 = ""
+    char2 = matr[e2r][e1c]
+
+    return char1, char2
+
+
+def encryptByPlayfairCipher(Matrix, plainList):
+    CipherText = []
+    for i in range(0, len(plainList)):
+        c1 = 0
+        c2 = 0
+        ele1_x, ele1_y = search(Matrix, plainList[i][0])
+        ele2_x, ele2_y = search(Matrix, plainList[i][1])
+
+        if ele1_x == ele2_x:
+            c1, c2 = encrypt_RowRule(Matrix, ele1_x, ele1_y, ele2_x, ele2_y)
+            # Get 2 letter cipherText
+        elif ele1_y == ele2_y:
+            c1, c2 = encrypt_ColumnRule(Matrix, ele1_x, ele1_y, ele2_x, ele2_y)
+        else:
+            c1, c2 = encrypt_RectangleRule(Matrix, ele1_x, ele1_y, ele2_x, ele2_y)
+
+        cipher = c1 + c2
+        CipherText.append(cipher)
+    return CipherText
+
+
+text_Plain = "instruments"
+text_Plain = removeSpaces(toLowerCase(text_Plain))
+PlainTextList = Diagraph(FillerLetter(text_Plain))
+if len(PlainTextList[-1]) != 2:
+    PlainTextList[-1] = PlainTextList[-1] + "z"
+
+key = "Monarchy"
+print("Key text:", key)
+key = toLowerCase(key)
+Matrix = generateKeyTable(key, list1)
+
+print("Plain Text:", text_Plain)
+CipherList = encryptByPlayfairCipher(Matrix, PlainTextList)
+
+CipherText = ""
+for i in CipherList:
+    CipherText += i
+print("CipherText:", CipherText)
+
+# This code is Contributed by Boda_Venkata_Nikith
